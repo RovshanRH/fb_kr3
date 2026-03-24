1a0408bb81de0cc6908604882d63409404b45747
RovshanRH <rovshanbro@gmail.com>
2026-03-24 10:34:42 +0300

    pr13-14 done

---
+++ sw.js
@@ -0,0 +1,42 @@
+// скрипт service worker
+const CACHE_NAME = 'notes-cache-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/icons/favicon-32x32.png',
+  '/icons/favicon-64x64.png',
+  '/icons/favicon-128x128.png',
+  '/icons/android-chrome-192x192.png',
+  '/icons/favicon-256x256.png',
+  '/icons/android-chrome-512x512.png',
+  '/icons/apple-touch-icon.png',
+  '/icons/favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
